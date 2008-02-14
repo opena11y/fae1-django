e22d739a3c1b85be33fbfeff612e822ec4e3a958
Nicholas Hoyt <nhoyt@illinois.edu>
2008-02-14 20:35:22 +0000

    reworked imports in urls.py

--- urls.py
+++ urls.py
@@ -1,28 +1,27 @@
 from django.conf.urls.defaults import *
-from fae.views import index, report, page_report, archived_reports, about, logout
+from fae import views
 
 urlpatterns = patterns('',
     # Default page
-    (r'^$', index),
-    (r'^multi/$', index, {'multi': True}),
-    (r'^evaluate/$', index, {'evaluate': True}),
+    (r'^$', views.index),
+    (r'^multi/$', views.index, {'multi': True}),
 
     # Report pages
-    (r'^report/([0-9a-f]{16})/$', report),
-    (r'^report/([0-9a-f]{16})/(summary|sitewide|page)/$', report),
-    (r'^report/([0-9a-f]{16})/(sitewide)/([a-z]+)/$', report),
-    (r'^report/([0-9a-f]{16})/(page)/(\d+)/$', page_report),
-    (r'^report/([0-9a-f]{16})/(page)/(\d+)/([a-z]+)/$', page_report),
+    (r'^report/([0-9a-f]{16})/$', views.report),
+    (r'^report/([0-9a-f]{16})/(summary|sitewide|page)/$', views.report),
+    (r'^report/([0-9a-f]{16})/(sitewide)/([a-z]+)/$', views.report),
+    (r'^report/([0-9a-f]{16})/(page)/(\d+)/$', views.page_report),
+    (r'^report/([0-9a-f]{16})/(page)/(\d+)/([a-z]+)/$', views.page_report),
 
     # Report archive
-    (r'^archive/$', archived_reports),
+    (r'^archive/$', views.archived_reports),
 
     # About FAE
-    (r'^about/$', about),
-    (r'^about/(\w+)/$', about),
+    (r'^about/$', views.about),
+    (r'^about/(\w+)/$', views.about),
 
     # Authentication
-    (r'^logout/$', logout),
+    (r'^logout/$', views.logout),
 
     # Next mapping expects login.html in templates/registration
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),

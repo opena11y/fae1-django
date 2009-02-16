a8d46bebbe5d62634571b1f67d2f506270e24f0c
Nicholas Hoyt <nhoyt@illinois.edu>
2009-02-16 21:27:19 +0000

    Added wget build info to installation doc; modified admin.py for UserReport list_display.

--- fae/admin.py
+++ fae/admin.py
@@ -4,6 +4,13 @@ from project.fae import models
 admin.site.register(models.Organization)
 admin.site.register(models.UserProfile)
 admin.site.register(models.EmailSuffix)
-admin.site.register(models.UserReport)
+
+class UserReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'timestamp', 'archive')
+    ordering = ('user',)
+    list_filter = ('archive', 'user')
+
+admin.site.register(models.UserReport, UserReportAdmin)
+
 admin.site.register(models.GuestReport)
 admin.site.register(models.UsageStats)
